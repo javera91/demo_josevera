d28c1be7518a8d3b61ea0a26a25e93ea73fe0252
javera91 <146044960+javera91@users.noreply.github.com>
2024-11-03 14:10:36 -0500

    c031124

--- src/main/kotlin/com/example/demo/controller/HomeController.kt
+++ src/main/kotlin/com/example/demo/controller/HomeController.kt
@@ -1,6 +1,9 @@
 package com.example.demo.controller
 
 import com.example.demo.model.Model
+import com.example.demo.response.ErrorResponse
+import com.example.demo.response.FailedResponse
+import com.example.demo.response.SuccessResponse
 import jakarta.websocket.server.PathParam
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
@@ -12,19 +15,27 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping("/home")
 class HomeController {
 
-    /*
-    @GetMapping("/{id}")
-    fun home(@PathVariable("id") id: Int): String{
-        return "Hello, World! $id"
+    @GetMapping()
+    fun answerOne(@RequestBody model: Model): SuccessResponse {
+        return SuccessResponse().apply {
+            status = "success"
+            data = model
+        }
     }
 
-    @GetMapping()
-    fun home(@PathParam ("id") id: Int): String{
-        return "Hello, World! $id"
+    @GetMapping("/failed")
+    fun answerTwo(@RequestBody model: Model): FailedResponse {
+        return FailedResponse().apply {
+            status = "failed"
+            data = "El campo nombre está sin información."
+        }
     }
-    */
-    @GetMapping()
-    fun home(@RequestBody model:Model): String{
-        return "Hello, World! ${model.id} ${model.name} ${model.age}"
+
+    @GetMapping("/error")
+    fun answerThree(@RequestBody model: Model): ErrorResponse {
+        return ErrorResponse().apply {
+            status = "error"
+            data = "Se produjo un error interno en el servidor."
+        }
     }
 }
